c6d5656a919ff361b8602daf14dd42bae6b6534d
Hayden Daly <hdaly1@stevens.edu>
2020-11-11 20:14:20 -0800

    docker-compose foundation

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div style={{ 
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <p>React App</p>
+    </div>
+  );
+}
+
+export default App;
