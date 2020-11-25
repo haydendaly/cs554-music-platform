5011fe72470408b1e5cb2db59e88afaa91baa4ce
miranda-zou <zouxianqing@gmail.com>
2020-11-25 16:33:28 -0500

    Add AuthProvider back
    Add AuthProvider back

--- frontend/src/app/index.js
+++ frontend/src/app/index.js
@@ -1,26 +1,29 @@
-import React from 'react'
-import { BrowserRouter as Router,Route,Switch } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { NavBar,Sidebar,SignIn,SignUp } from '../components'
-import { PostInsert, Home} from '../pages'
+import { NavBar, Sidebar, SignIn, SignUp } from "../components";
+import { PostInsert, Home } from "../pages";
+import { AuthProvider } from "../firebase/Auth";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-    return (
-        <Router>
-            <NavBar />
-            <Sidebar/>
+  return (
+    <AuthProvider>
+      <Router>
+        <NavBar />
+        <Sidebar />
 
-            <Switch>               
-                <Route path="/post/create" exact component={PostInsert} /> 
-                <Route path="/post" exact component={Home}/> 
-                {/* <Route path="*" exact component={Home}/>  */}
-                <Route path="/signin" component={SignIn} />
-                <Route path="/signup" component={SignUp} />                     
-            </Switch>
-        </Router>
-    )
+        <Switch>
+          <Route path="/post/create" exact component={PostInsert} />
+          <Route path="/post" exact component={Home} />
+          {/* <Route path="*" exact component={Home}/>  */}
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+        </Switch>
+      </Router>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
