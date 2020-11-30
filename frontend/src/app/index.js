f7a169edc97d7411150b3a7f0170a9f651318709
Hayden Daly <hdaly1@stevens.edu>
2020-11-30 00:57:28 -0800

    Codesplitting and docker-compose edits

--- frontend/src/app/index.js
+++ frontend/src/app/index.js
@@ -1,25 +1,24 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   Redirect,
 } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import {
-  NavBar,
-  Sidebar,
-  SidebarRight,
-  SignIn,
-  SignUp,
-  PlayAlbum,
-  PlayListByLoveSongs,
-} from "../components";
-import { PostInsert, Home } from "../pages";
 import { AuthProvider } from "../firebase/Auth";
 // import { PlayAlbum } from "../components";
+import NavBar from "../components/NavBar";
+import Sidebar from "../components/Sidebar";
+import SidebarRight from "../components/SidebarRight";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+const SignIn = lazy(() => import("../components/SignIn"));
+const SignUp = lazy(() => import("../components/SignUp"));
+const PlayAlbum = lazy(() => import("../components/playAlbum"));
+const PlayListByLoveSongs = lazy(() => import("../components/playListbyLoveSongs"));
+const PostInsert = lazy(() => import("../pages/PostInsert"));
+const Home = lazy(() => import("../pages/Home"));
 
 function App() {
   return (
@@ -28,17 +27,19 @@ function App() {
         <NavBar />
         <Sidebar />
         <SidebarRight />
-        <Switch>
-          <Route path="/post/create" exact component={PostInsert} />
-          <Route path="/post" exact component={Home} />
-          <Route path="/signin" component={SignIn} />
-          <Route path="/signup" component={SignUp} />
-          <Route path="/playList" exact component={PlayAlbum} />
-          <Route path="/playListbyLove" exact component={PlayListByLoveSongs} />
-          <Route path="/">
-            <Redirect to="/post" />
-          </Route>
-        </Switch>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <Route path="/post/create" exact component={PostInsert} />
+            <Route path="/post" exact component={Home} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/playList" exact component={PlayAlbum} />
+            <Route path="/playListbyLove" exact component={PlayListByLoveSongs} />
+            <Route path="/">
+              <Redirect to="/post" />
+            </Route>
+          </Switch>
+        </Suspense>
       </Router>
     </AuthProvider>
   );
