4bdb816b0dec2fcc0dc41c51ad53f7bb23e0029f
Hayden Daly <hdaly1@stevens.edu>
2020-12-04 14:04:20 -0800

    Added format script and responsive sidenavs

--- frontend/src/app/index.js
+++ frontend/src/app/index.js
@@ -1,48 +1,58 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense } from 'react'
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect,
+} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-import { AuthProvider } from "../firebase/Auth";
+import { AuthProvider } from '../firebase/Auth'
 // import { PlayAlbum } from "../components";
-import NavBar from "../components/NavBar";
-import Sidebar from "../components/Sidebar";
-import SidebarRight from "../components/SidebarRight";
+import NavBar from '../components/NavBar'
+import Sidebar from '../components/Sidebar'
+import SidebarRight from '../components/SidebarRight'
 
-const SignIn = lazy(() => import("../components/SignIn"));
-const SignUp = lazy(() => import("../components/SignUp"));
-const PlayAlbum = lazy(() => import("../components/playAlbum"));
-const PlayListByLoveSongs = lazy(() => import("../components/playListbyLoveSongs"));
-const PostInsert = lazy(() => import("../pages/PostInsert"));
-const Home = lazy(() => import("../pages/Home"));
+const SignIn = lazy(() => import('../components/SignIn'))
+const SignUp = lazy(() => import('../components/SignUp'))
+const PlayAlbum = lazy(() => import('../components/playAlbum'))
+const PlayListByLoveSongs = lazy(() =>
+    import('../components/playListbyLoveSongs')
+)
+const PostInsert = lazy(() => import('../pages/PostInsert'))
+const Home = lazy(() => import('../pages/Home'))
 
 function App() {
-  return (
-    <AuthProvider>
-      <Router>
-        <NavBar />
-        <Sidebar />
-        <SidebarRight />
-        <Suspense fallback={<p>Loading...</p>}>
-          <Switch>
-            <Route path="/post/create" exact component={PostInsert} />
-            <Route path="/post" exact component={Home} />
-            <Route path="/signin" component={SignIn} />
-            <Route path="/signup" component={SignUp} />
-            <Route path="/playList" exact component={PlayAlbum} />
-            <Route path="/playListbyLove" exact component={PlayListByLoveSongs} />
-            <Route path="/">
-              <Redirect to="/post" />
-            </Route>
-          </Switch>
-        </Suspense>
-      </Router>
-    </AuthProvider>
-  );
+    return (
+        <AuthProvider>
+            <Router>
+                <NavBar />
+                <Sidebar />
+                <SidebarRight />
+                <Suspense fallback={<p>Loading...</p>}>
+                    <Switch>
+                        <Route
+                            path="/post/create"
+                            exact
+                            component={PostInsert}
+                        />
+                        <Route path="/post" exact component={Home} />
+                        <Route path="/signin" component={SignIn} />
+                        <Route path="/signup" component={SignUp} />
+                        <Route path="/playList" exact component={PlayAlbum} />
+                        <Route
+                            path="/playListbyLove"
+                            exact
+                            component={PlayListByLoveSongs}
+                        />
+                        <Route path="/">
+                            <Redirect to="/post" />
+                        </Route>
+                    </Switch>
+                </Suspense>
+            </Router>
+        </AuthProvider>
+    )
 }
 
-export default App;
+export default App
--- frontend/src/components/SignOut.js
+++ frontend/src/components/SignOut.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { doSignOut } from "../firebase/FirebaseFunctions";
+import React from 'react'
+import { doSignOut } from '../firebase/FirebaseFunctions'
 
 const SignOutButton = () => {
-  return (
-    <button type="button" onClick={doSignOut} className="btn btn-primary">
-      Sign Out
-    </button>
-  );
-};
+    return (
+        <button type="button" onClick={doSignOut} className="btn btn-primary">
+            Sign Out
+        </button>
+    )
+}
 
-export default SignOutButton;
+export default SignOutButton
