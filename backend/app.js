368e73079f29d9a874847070aad34d60ba612048
Kyle Gensheimer <kylegensheimer@gmail.com>
2020-12-11 01:59:07 -0500

    fix package & package-lock

--- backend/app.js
+++ backend/app.js
@@ -12,4 +12,9 @@ app.use(express.json());
 
 configRoutes(app);
 
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log("Your routes will be running on http://localhost:3000");
+});
+
 module.exports = app;
