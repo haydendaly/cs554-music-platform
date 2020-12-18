c4a512100c5497a9778e2774b341d6440d7caf6f
miranda-zou <zouxianqing@gmail.com>
2020-12-17 22:37:40 -0500

    auto format

--- frontend/src/pages/PostInsert.jsx
+++ frontend/src/pages/PostInsert.jsx
@@ -358,7 +358,14 @@ function PostInsert() {
                             <div className="post-list">
                                 {openComment && openComment === postItem._id && (
                                     <div className="post-comment-holder">
-                                        <label htmlFor={'commentField' + postItem._id} hidden>Comment</label>
+                                        <label
+                                            htmlFor={
+                                                'commentField' + postItem._id
+                                            }
+                                            hidden
+                                        >
+                                            Comment
+                                        </label>
                                         <textarea
                                             className="post-comments shadow"
                                             id={'commentField' + postItem._id}
